679496bf7abba4e15441f83eae4ca8de18c21d54
Nazeer <jemsynazeer6@gmail.com>
2018-09-03 16:29:23 +0530

    Added my first code for recative form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
+import { formControlBinding } from '../../node_modules/@angular/forms/src/directives/ng_model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  userForm: FormGroup;
+  constructor(){
+    this.userForm = new FormGroup({
+      'name': new FormControl(),
+      'email': new FormControl(),
+      'gender': new FormControl(),
+      'favFood': new FormArray([
+        new FormGroup({
+          'indian': new FormControl()
+        }),
+        new FormGroup({
+          'chinese': new FormControl()
+        })
+      ]),
+      'addressess': new FormArray([
+       this.createAddress()
+       
+      ])
+    });
+  }
+  createAddress(){
+    return  new FormGroup({
+      'line1': new FormControl(),
+      'line2': new FormControl(),
+      'country': new FormControl(),
+      'state': new FormControl(),
+      'city': new FormControl()
+    })
+  }
+  addAddress(){
+    let myAddress = this.userForm.get('addressess') as FormArray; 
+    myAddress.push(this.createAddress());
+  }
 }
